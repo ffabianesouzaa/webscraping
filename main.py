c1e1138ca75d42120087d53fd9a473a46b526847
ffabianesouzaa <ffabianesouzaa@gmail.com>
2023-01-31 17:46:32 -0300

    After changes in scrape of city

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from time import sleep, time
 from random import randint
 from warnings import warn
 from requests import get
-from IPython.display import clear_output
 from bs4 import BeautifulSoup as bs
 
 import database
@@ -14,10 +13,10 @@ start_time = time()
 requests = 0
 
 # For every page
-pages = [str(i) for i in range(1, 7)]
+pages = [str(i) for i in range(1, 6)]
 for page in pages:
 
-    # Connection verification
+    # Internet connection verification
     connected = False
     while connected is not True:
         try:
@@ -31,7 +30,7 @@ for page in pages:
             sleep(20)
 
     # Make a get request
-    response = get('https://www.trabalhabrasil.com.br/vagas-empregos-em-sao-paulo-sp?pagina=' + page)
+    response = get('https://www.trabalhabrasil.com.br/vagas-empregos-em-francisco-morato-sp?pagina=' + page)
 
     # Pause the loop
     sleep(randint(8, 15))
@@ -40,14 +39,13 @@ for page in pages:
     requests += 1
     elapsed_time = time() - start_time
     print('Request:{}; Frequency: {} requests/s'.format(requests, requests / elapsed_time))
-    clear_output(wait=True)
 
     # Throw a warning for non-200 status codes
     if response.status_code != 200:
         warn('Request: {}; Status code: {}'.format(requests, response.status_code))
 
     # Break the loop if the number of requests is greater than expected
-    if requests > 6:
+    if requests > 5:
         warn('Number of requests was greater than expected.')
         break
 
@@ -60,38 +58,56 @@ for page in pages:
     for container in jobsContainers:
         if container.find('div', class_='jg__job') is not None:
 
-            # Scrape title
-            # Selecting the HTML
-            titles = soup.select('.job__name')
-            listTitle = []
-            for title in titles:
-                # Add only the text to a list
-                listTitle.append(title.get_text(strip=True))
-
-            # Scrape quantity
-            quantities = soup.select('.job__name')
-            listQuantity = []
-            for quantity in quantities:
-                listQuantity.append(quantity.get_text(strip=True))
-
-            # Scrape company
-            companies = soup.select('.job__company')
-            listCompany = []
-            for company in companies:
-                listCompany.append(company.get_text(strip=True))
-
             # Scrape city
             cities = soup.select('.job__detail')
             listCity = []
             for city in cities:
-                listCity.append(city.get_text(strip=True))
+                if 'Francisco Morato/SP' in city:
+                    listCity.append(city.get_text(strip=True))
 
-            # Scrape state
-            states = soup.select('.job__detail')
-            listState = []
-            for state in states:
-                listState.append(state.get_text(strip=True))
+            # If the city name change in the page
+            if len(listCity) == 0:
+                exit()
+            else:
 
+                # Scrape title
+                titles = soup.select('.job__name')
+                listTitle = []
+                cont = 0
+                for title in titles:
+                    if cont < len(listCity):
+                        listTitle.append(title.get_text(strip=True))
+                        cont += 1
+
+                # Scrape quantity
+                quantities = soup.select('.job__name')
+                listQuantity = []
+                cont = 0
+                for quantity in quantities:
+                    if cont < len(listCity):
+                        listQuantity.append(quantity.get_text(strip=True))
+                        cont += 1
+
+                # Scrape company
+                cont = 0
+                companies = soup.select('.job__company')
+                listCompany = []
+                for company in companies:
+                    if cont < len(listCity):
+                        listCompany.append(company.get_text(strip=True))
+                        cont += 1
+
+                # Scrape state
+                cont = 0
+                states = soup.select('.job__detail')
+                listState = []
+                for state in states:
+                    if cont < len(listCity):
+                        if 'Francisco Morato/SP' in state:
+                            listState.append(state.get_text(strip=True))
+                            cont += 1
+
+        # Applyng functions and save in database
         database.savedb(functions.quantity(listQuantity), functions.title(listTitle),
                         functions.company(listCompany), functions.city(listCity),
                         functions.state(listState))
